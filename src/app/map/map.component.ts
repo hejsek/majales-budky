a5acfa5c7a004bdfc10ef903111b286065054a21
Jakub Heyduk <heyduk2@seznam.cz>
2017-05-01 21:16:21 +0200

    Naroubování map na Angular

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import '../../assets/libs/map.js';
+
+import {MapController} from "../map-controller"
+
+declare var map;
+
 
 @Component({
   selector: 'app-map',
@@ -10,6 +16,14 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // main();
+  }
+
+
+  refreshData() {
+    // console.log(map);
+    MapController.refreshData();
+    console.log(window);
   }
 
 }
